12d4207d108cba861c29012e1871a548fa53546b
Omer Re <39154767+omer-re@users.noreply.github.com>
2021-07-11 19:31:00 +0300

    Add files via upload

--- ytdl_courses_script.py
+++ ytdl_courses_script.py
@@ -1,9 +1,40 @@
 ## MAKE SURE youtube-dl.exe IS IN YOUR ENVIRONMENT PATH!
 import os	
 import subprocess
+
+# check if tqdm exists, otherwise install it
+import sys
+import subprocess
+import pkg_resources
+
+installed = {pkg.key for pkg in pkg_resources.working_set}
+
+if 'tqdm' not in installed:
+    python = sys.executable
+    subprocess.check_call([python, '-m', 'pip', 'install', 'tqdm'], stdout=subprocess.DEVNULL)
+
+else:
+    print('TQDM exists')
+
 import tqdm
 
+
 PATH=os.getcwd()
+
+# before changing dir, check for 'youtube-dl' in the project folder
+try:
+    # Try calling ABC here anyway you like
+    # Here I am just printing it
+	os.system('youtube-dl.exe')
+except NameError:
+    print("Variable youtube-dl.exe does not exist")
+	if os.path.isfile("youtube-dl.exe"):    # case youtube-dl isn't on ENVIRONMENT VARIABLES but is in the folder
+	    YTDL_PATH=os.path.abspath("youtube-dl.exe")
+	else:
+	    print("You are missing youtube-dl.exe or it isn't configured in your environment path properly.\nPlease fix and try again.")
+	    exit(0)
+
+
 os.chdir(PATH)
 YTDL_PATH=""
 
@@ -20,18 +51,8 @@ NAME_OF_COURSE=URL_LIST_FILE.replace('.txt',"") #enter path or just the name if 
 NAME_OF_COURSE=NAME_OF_COURSE.strip()
 NAME_OF_COURSE=NAME_OF_COURSE.replace(" ","_")
 
-check_ytdl=subprocess.getoutput('youtube-dl.exe')
-if "not recognized" in check_ytdl:
-	if os.path.isfile("youtube-dl.exe"):    # case youtube-dl isn't on ENVIRONMENT VARIABLES but is in the folder
-	    YTDL_PATH=os.path.abspath("youtube-dl.exe")
-	else:
-	    print("You are missing youtube-dl.exe or it isn't configured in your environment path properly.\nPlease fix and try again.")
-	    exit(0)
 
-else:
-	print("youtube-dl.exe is valid")
-
-
+# parse lines
 lines=[]
 with open(URL_LIST_FILE) as f:
     lines=(f.readlines())
